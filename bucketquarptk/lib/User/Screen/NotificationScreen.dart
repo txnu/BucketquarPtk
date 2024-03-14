651b20e674831d8b921d4bddfb99a10fe943f479
TANU WIJAYA <tanuxvi2@gmail.com>
2024-03-15 03:14:00 +0700

    Penambahan NotificationScreen

--- bucketquarptk/lib/User/Screen/NotificationScreen.dart
+++ bucketquarptk/lib/User/Screen/NotificationScreen.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
+import '../Widget/NotificationWidget.dart';
+
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(120),
-      child: Text(
-        "Notification",
-        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            height: 700,
+            padding: EdgeInsets.only(top: 15),
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35),
+              ),
+            ),
+            child: Column(
+              children: [
+                NotificationWidget(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ bucketquarptk/lib/User/Widget/NotificationWidget.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+class NotificationWidget extends StatelessWidget {
+  const NotificationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: EdgeInsets.only(left: 18),
+          child: Text(
+            "Today",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          height: 110,
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                child: Icon(
+                  Icons.notifications_active,
+                  size: 35,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Produk anda telah dikirimkan",
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "5 menit yang lalu",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 80,
+                width: 80,
+                child: Image.asset(
+                    'assets/images/buketwisuda1-removebg-preview-2-p4C.png'),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 18),
+          child: Text(
+            "Kemarin",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          height: 110,
+          margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                child: Icon(
+                  Icons.notifications_active,
+                  size: 35,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Produk anda telah dikirimkan",
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Kemarin",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 80,
+                width: 80,
+                child: Image.asset(
+                    'assets/images/buketwisuda1-removebg-preview-2-p4C.png'),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
